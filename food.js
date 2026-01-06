73c24e92ed8d27d0682900a4775185cb3eeb14e2
Shane Diola <shanebobadilladiola@gmail.com>
2026-01-06 15:48:40 +0800

    add

---
+++ food.js
@@ -0,0 +1,440 @@
+const menu ={
+ "appetizers": [
+    {
+      "foodId": "APP001",
+      "category": "Appetizer",
+      "name": "Canapés",
+      "description": "Assorted bite-sized appetizers topped with fresh ingredients.",
+      "price": 45,
+      "servingSize": "2–3 pcs",
+      "isAvailable": "true",
+      "image": "canapes.jpg"
+    },
+    {
+      "foodId": "APP002",
+      "category": "Appetizer",
+      "name": "Cheese Sticks",
+      "description": "Crispy fried cheese sticks served with dipping sauce.",
+      "price": 40,
+      "servingSize": "2 pcs",
+      "isAvailable": "true",
+      "image": "cheeseSticks.jpg"
+    },
+    {
+      "foodId": "APP003",
+      "category": "Appetizer",
+      "name": "Fresh Lumpia",
+      "description": "Fresh Filipino spring rolls filled with vegetables and sauce.",
+      "price": 50,
+      "servingSize": "1–2 pcs",
+      "isAvailable": "true",
+      "image": "freshLumpia.jpg"
+    },
+    {
+      "foodId": "APP004",
+      "category": "Appetizer",
+      "name": "Hotdog Sticks",
+      "description": "Battered hotdogs on sticks, deep-fried until golden.",
+      "price": 35,
+      "servingSize": "1–2 pcs",
+      "isAvailable": "true",
+      "image": "hotdogSticks.jpg"
+    },
+    {
+      "foodId": "APP005",
+      "category": "Appetizer",
+      "name": "Lumpiang Shanghai",
+      "description": "Crispy spring rolls filled with seasoned ground meat.",
+      "price": 55,
+      "servingSize": "3–4 pcs",
+      "isAvailable": "true",
+      "image": "lumpiangShanghai.jpg"
+    },
+    {
+      "foodId": "APP006",
+      "category": "Appetizer",
+      "name": "Mini Quiche",
+      "description": "Bite-sized savory quiches with egg and cheese filling.",
+      "price": 60,
+      "servingSize": "1–2 pcs",
+      "isAvailable": "true",
+      "image": "miniQuiche.jpg"
+    },
+    {
+      "foodId": "APP007",
+      "category": "Appetizer",
+      "name": "Nachos",
+      "description": "Tortilla chips topped with cheese, salsa, and garnishes.",
+      "price": 70,
+      "servingSize": "50–70 g",
+      "isAvailable": "true",
+      "image": "nachos.jpg"
+    },
+    {
+      "foodId": "APP008",
+      "category": "Appetizer",
+      "name": "Chicken Nuggets",
+      "description": "Golden fried bite-sized chicken pieces.",
+      "price": 45,
+      "servingSize": "3–4 pcs",
+      "isAvailable": "true",
+      "image": "nuggets.jpg"
+    },
+    {
+      "foodId": "APP009",
+      "category": "Appetizer",
+      "name": "Onion Rings",
+      "description": "Crispy battered onion rings served hot.",
+      "price": 40,
+      "servingSize": "4–5 pcs",
+      "isAvailable": "true",
+      "image": "onionRings.jpg"
+    },
+    {
+      "foodId": "APP010",
+      "category": "Appetizer",
+      "name": "Pork Siomai",
+      "description": "Steamed dumplings filled with seasoned pork.",
+      "price": 55,
+      "servingSize": "3 pcs",
+      "isAvailable": "true",
+      "image": "porkSiomai.jpg"
+    },
+    {
+      "foodId": "APP011",
+      "category": "Appetizer",
+      "name": "Shrimp Tempura",
+      "description": "Lightly battered shrimp fried until crispy.",
+      "price": 80,
+      "servingSize": "2 pcs",
+      "isAvailable": "true",
+      "image": "shrimpTempura.jpg"
+    },
+    {
+      "foodId": "APP012",
+      "category": "Appetizer",
+      "name": "Skewers",
+      "description": "Grilled meat skewers glazed with savory sauce.",
+      "price": 65,
+      "servingSize": "1–2 sticks",
+      "isAvailable": "true",
+      "image": "skewers.jpg"
+    },
+    {
+      "foodId": "APP013",
+      "category": "Appetizer",
+      "name": "Stuffed Mushrooms",
+      "description": "Mushroom caps filled with seasoned stuffing and cheese.",
+      "price": 60,
+      "servingSize": "2 pcs",
+      "isAvailable": "true",
+      "image": "stuffedMushrooms.jpg"
+    },
+    {
+      "foodId": "APP014",
+      "category": "Appetizer",
+      "name": "Cheese Crackers",
+      "description": "Crackers topped with cheese and fruit garnish.",
+      "price": 35,
+      "servingSize": "3–4 pcs",
+      "isAvailable": "true",
+      "image": "cheeseCrackers.jpg"
+    },
+    {
+      "foodId": "APP015",
+      "category": "Appetizer",
+      "name": "French Fries",
+      "description": "Classic crispy fried potato strips.",
+      "price": 30,
+      "servingSize": "80–100 g",
+      "isAvailable": "true",
+      "image": "frenchFries.jpg"
+    },
+    {
+      "foodId": "APP016",
+      "category": "Appetizer",
+      "name": "Mushroom Soup",
+      "description": "Creamy soup made from fresh mushrooms.",
+      "price": 50,
+      "servingSize": "1 cup (200 ml)",
+      "isAvailable": "true",
+      "image": "mushroomSoup.jpg"
+    }
+  ],
+ "mainCourses":
+  [
+  {
+    "foodId": "MC001",
+    "category": "Main Course",
+    "name": "Caldereta",
+    "description": "A rich Filipino stew made with tender meat, tomatoes, and vegetables.",
+    "price": 180,
+    "servingSize": "1 plate (350 g)",
+    "isAvailable": "true",
+    "image": "caldereta.jpg"
+  },
+  {
+    "foodId": "MC002",
+    "category": "Main Course",
+    "name": "Fish Fillet",
+    "description": "Lightly seasoned fish fillet, pan-fried to golden perfection.",
+    "price": 150,
+    "servingSize": "1 fillet (200 g)",
+    "isAvailable": "true",
+    "image": "fishFillet.jpg"
+  },
+  {
+    "foodId": "MC003",
+    "category": "Main Course",
+    "name": "Roast Chicken",
+    "description": "Whole chicken roasted with herbs and spices.",
+    "price": 320,
+    "servingSize": "1 whole chicken (1200 g)",
+    "isAvailable": "true",
+    "image": "roastChicken.jpg"
+  },
+  {
+    "foodId": "MC004",
+    "category": "Main Course",
+    "name": "Bistek",
+    "description": "Filipino beef steak cooked with soy sauce, calamansi, and onions.",
+    "price": 190,
+    "servingSize": "1 plate (300 g)",
+    "isAvailable": "true",
+    "image": "bistek.jpg"
+  },
+  {
+    "foodId": "MC005",
+    "category": "Main Course",
+    "name": "Butter Shrimp",
+    "description": "Shrimp cooked in a rich and savory butter sauce.",
+    "price": 220,
+    "servingSize": "1 plate (280 g)",
+    "isAvailable": "true",
+    "image": "butterShrimp.jpg"
+  },
+  {
+    "foodId": "MC006",
+    "category": "Main Course",
+    "name": "Cheese Pizza",
+    "description": "Classic pizza topped with melted cheese on a crispy crust.",
+    "price": 250,
+    "servingSize": "8 slices (700 g)",
+    "isAvailable": "true",
+    "image": "cheesePizza.jpg"
+  },
+  {
+    "foodId": "MC007",
+    "category": "Main Course",
+    "name": "Chicken Adobo",
+    "description": "Chicken simmered in soy sauce, vinegar, garlic, and spices.",
+    "price": 170,
+    "servingSize": "1 plate (320 g)",
+    "isAvailable": "true",
+    "image": "chickenAdobo.jpg"
+  },
+  {
+    "foodId": "MC008",
+    "category": "Main Course",
+    "name": "Chicken Afritada",
+    "description": "Chicken stewed in tomato sauce with potatoes and carrots.",
+    "price": 170,
+    "servingSize": "1 plate (330 g)",
+    "isAvailable": "true",
+    "image": "chickenAfritada.jpg"
+  },
+  {
+    "foodId": "MC009",
+    "category": "Main Course",
+    "name": "Chicken Wings",
+    "description": "Crispy chicken wings tossed in savory sauce.",
+    "price": 160,
+    "servingSize": "6 pieces (360 g)",
+    "isAvailable": "true",
+    "image": "chickenWings.jpg"
+  },
+  {
+    "foodId": "MC010",
+    "category": "Main Course",
+    "name": "Cordon Bleu",
+    "description": "Breaded chicken stuffed with ham and cheese.",
+    "price": 200,
+    "servingSize": "1 roll (280 g)",
+    "isAvailable": "true",
+    "image": "cordonBleu.jpg"
+  },
+  {
+    "foodId": "MC011",
+    "category": "Main Course",
+    "name": "Egg Crabs",
+    "description": "Crabs cooked with flavorful sauce and rich crab roe.",
+    "price": 280,
+    "servingSize": "1 plate (450 g)",
+    "isAvailable": "true",
+    "image": "eggCrabs.jpg"
+  },
+  {
+    "foodId": "MC012",
+    "category": "Main Course",
+    "name": "Lechon Belly",
+    "description": "Crispy roasted pork belly with juicy meat inside.",
+    "price": 260,
+    "servingSize": "1 plate (300 g)",
+    "isAvailable": "true",
+    "image": "lechonBelly.jpg"
+  },
+  {
+    "foodId": "MC013",
+    "category": "Main Course",
+    "name": "Mac and Cheese",
+    "description": "Creamy macaroni pasta baked with melted cheese.",
+    "price": 140,
+    "servingSize": "1 bowl (250 g)",
+    "isAvailable": "true",
+    "image": "maccheese.jpg"
+  },
+  {
+    "foodId": "MC014",
+    "category": "Main Course",
+    "name": "Menudo",
+    "description": "Filipino pork stew with tomato sauce, potatoes, and carrots.",
+    "price": 180,
+    "servingSize": "1 plate (340 g)",
+    "isAvailable": "true",
+    "image": "menudo.jpg"
+  },
+  {
+    "foodId": "MC015",
+    "category": "Main Course",
+    "name": "Pork Liempo",
+    "description": "Grilled marinated pork belly served juicy and flavorful.",
+    "price": 220,
+    "servingSize": "1 plate (280 g)",
+    "isAvailable": "true",
+    "image": "porkLiempo.jpg"
+  },
+  {
+    "foodId": "MC016",
+    "category": "Main Course",
+    "name": "Spaghetti",
+    "description": "Classic spaghetti pasta topped with rich tomato sauce.",
+    "price": 130,
+    "servingSize": "1 plate (300 g)",
+    "isAvailable": "true",
+    "image": "spaghetti.jpg"
+  }
+],
+ "desserts":
+[
+  {
+    "foodId": "DS001",
+    "category": "Dessert",
+    "name": "Puto",
+    "description": "Soft and fluffy Filipino steamed rice cakes.",
+    "price": 60,
+    "servingSize": "4 pieces (200 g)",
+    "isAvailable": "true",
+    "image": "puto.jpg"
+  },
+  {
+    "foodId": "DS002",
+    "category": "Dessert",
+    "name": "Buko Pandan",
+    "description": "Creamy dessert made with young coconut, pandan jelly, and sweet cream.",
+    "price": 80,
+    "servingSize": "1 bowl (250 g)",
+    "isAvailable": "true",
+    "image": "bukoPandan.jpg"
+  },
+  {
+    "foodId": "DS003",
+    "category": "Dessert",
+    "name": "Buko Salad",
+    "description": "Refreshing Filipino fruit salad with young coconut and cream.",
+    "price": 90,
+    "servingSize": "1 bowl (280 g)",
+    "isAvailable": "true",
+    "image": "bukoSalad.jpg"
+  },
+  {
+    "foodId": "DS004",
+    "category": "Dessert",
+    "name": "Cake",
+    "description": "Moist layered cake with rich chocolate frosting.",
+    "price": 120,
+    "servingSize": "1 slice (150 g)",
+    "isAvailable": "true",
+    "image": "cake.jpg"
+  },
+  {
+    "foodId": "DS005",
+    "category": "Dessert",
+    "name": "Coffee Jelly",
+    "description": "Coffee-flavored jelly mixed with cream and sugar.",
+    "price": 70,
+    "servingSize": "1 cup (220 g)",
+    "isAvailable": "true",
+    "image": "coffeeJelly.jpg"
+  },
+  {
+    "foodId": "DS006",
+    "category": "Dessert",
+    "name": "Cookies",
+    "description": "Freshly baked cookies with chocolate chips.",
+    "price": 50,
+    "servingSize": "3 pieces (120 g)",
+    "isAvailable": "true",
+    "image": "cookies.jpg"
+  },
+  {
+    "foodId": "DS007",
+    "category": "Dessert",
+    "name": "Cupcakes",
+    "description": "Mini cakes topped with creamy frosting.",
+    "price": 65,
+    "servingSize": "2 pieces (160 g)",
+    "isAvailable": "true",
+    "image": "cupcakes.jpg"
+  },
+  {
+    "foodId": "DS008",
+    "category": "Dessert",
+    "name": "Ice Cream",
+    "description": "Creamy frozen dessert available in assorted flavors.",
+    "price": 75,
+    "servingSize": "2 scoops (180 g)",
+    "isAvailable": "true",
+    "image": "iceCream.jpg"
+  },
+  {
+    "foodId": "DS009",
+    "category": "Dessert",
+    "name": "Maja Blanca",
+    "description": "Creamy coconut pudding topped with toasted corn.",
+    "price": 70,
+    "servingSize": "1 slice (200 g)",
+    "isAvailable": "true",
+    "image": "majaBlanca.jpg"
+  },
+  {
+    "foodId": "DS010",
+    "category": "Dessert",
+    "name": "Ube Halaya",
+    "description": "Sweet and creamy purple yam dessert.",
+    "price": 85,
+    "servingSize": "1 slice (220 g)",
+    "isAvailable": "true",
+    "image": "ubeHalaya.jpg"
+  },
+  {
+    "foodId": "DS011",
+    "category": "Dessert",
+    "name": "Leche Flan",
+    "description": "Classic caramel custard dessert with a silky texture.",
+    "price": 90,
+    "servingSize": "1 slice (180 g)",
+    "isAvailable": "true",
+    "image": "lecheFlan.jpg"
+  }
+]
+}
