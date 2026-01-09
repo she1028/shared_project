f314ea29445da38b2b664fd20d2b281e76a8e31b
ksstn <kristinemaglinao06@gmail.com>
2026-01-10 03:33:29 +0800

    include modular nav

--- children_party.php
+++ children_party.php
@@ -71,27 +71,7 @@
 
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg bg-light navbar-light fixed-top shadow-sm mt-3 rounded-4 mx-4">
-        <div class="container-fluid ps-4 pe-4">
-            <a class="navbar-brand" href="index.html">
-                <img src="images/YMZM-logo.png" alt="Logo" width="40" height="auto">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link px-3 text-dark" href="#home">Home</a></li>
-                    <li class="nav-item"><a class="nav-link px-3 text-dark" href="#packages">Packages</a></li>
-                    <li class="nav-item"><a class="nav-link px-3 text-dark" href="#menu">Menu</a></li>
-                    <li class="nav-item"><a class="nav-link px-3 text-dark" href="#rentals">Rentals</a></li>
-                    <li class="nav-item"><a class="nav-link px-3 text-dark" href="#contact">Contact</a></li>
-                    <li class="nav-item"><a class="nav-link px-3 text-dark border border-primary rounded-5"
-                            href="#sign-in">Sign in</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include("nav.php"); ?>
 
     <!-- Hero Section -->
     <section id="home">
