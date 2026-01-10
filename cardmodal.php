1a8a4277f4ddbb46931eb11681de0c8a455ffbd9
stephenyvelasco <ma.stepheny.velasco@gmail.com>
2026-01-10 15:15:13 +0800

    with back buttons

--- cardmodal.php
+++ cardmodal.php
@@ -11,6 +11,7 @@
     .back-action {
         cursor: pointer;
         user-select: none;
+        width: fit-content;
     }
 
     .qty-box {
@@ -52,9 +53,11 @@
                     </div>
                     <!-- Back button -->
                     <div class="col-lg-7 col-12 p-2 mt-2">
-                        <div class="d-flex align-items-center back-action g-2" data-bs-dismiss="modal">
-                            <i class="material-icons">&#xe5c4;</i>
-                            <span>back</span>
+                        <div class="m-2">
+                            <span class="d-inline-flex align-items-center back-action g-2" data-bs-dismiss="modal">
+                                <i class="material-icons">&#xe5c4;</i>
+                                <span>back</span>
+                            </span>
                         </div>
                         <!-- Category -->
                         <div class="d-flex align-items-center justify-content-center m-2">
