8178c6f92bbad8791d1b0402a11098b6b27898dc
Shella May Cuenca <shellamaycuenca12@gmail.com>
2026-01-14 21:44:09 +0800

    updated code

--- connect.php
+++ connect.php
@@ -3,11 +3,12 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";      // set to your MySQL root password if it has one
 $dbname = "ymzm";
+$port   = 3307;
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 try {
-  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $port);
   $conn->set_charset("utf8mb4");
 } catch (mysqli_sql_exception $e) {
   die("Database connection failed: " . $e->getMessage());
