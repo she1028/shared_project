cd88ee837c09a637cf6f0ccc2b1f03e68eb7070a
SarahCassandra <macalilongsarahcassandra@gmail.com>
2026-01-15 16:42:19 +0800

    Rest API

--- connect.php
+++ connect.php
@@ -3,12 +3,11 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";      // set to your MySQL root password if it has one
 $dbname = "ymzm";
-$port   = 3307;
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 try {
-  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $port);
+  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
   $conn->set_charset("utf8mb4");
 } catch (mysqli_sql_exception $e) {
   die("Database connection failed: " . $e->getMessage());
